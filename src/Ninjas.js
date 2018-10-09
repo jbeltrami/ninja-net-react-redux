b9bd2cbe4c75e4beac977f0b87ec5927e35086a1
jbeltrami <joao@primarydesign.com>
2018-10-09 15:15:54 -0400

    Add Remove Ninjas function.

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => {
+const Ninjas = ({ninjas, deleteNinja}) => {
   const ninjaList = ninjas.map(ninja => {
-    return ninja.age > 20
+    return ninja.age > 10
       ? (
         <div className="ninja" key={ninja.id}>
           <div>Name: {ninja.name}</div>
           <div>Age: {ninja.age}</div>
           <div>Belt: {ninja.belt}</div>
+          <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
         </div>
     ) : null
   })

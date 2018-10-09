45d21d0d91b08f632e759dd456c82be7d29ec21d
jbeltrami <joao@primarydesign.com>
2018-10-09 13:20:35 -0400

    Container component x UI component concept.
    - Container components are React Classes, that can be instanciated 
    multiple times and are used to pass props/states to nested components.
    
    - UI Components don't need to be classes. They're also known as function 
    components and are responsible for UI factors (how to display data).The 
    data is passed to them by the container component.

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-  render () {
-    // console.log('what is this?', this)
-    const { ninjas } = this.props;
+const Ninjas = ({ninjas}) => {
+  const ninjaList = ninjas.map(ninja => {
+    return (<div className="ninja" key={ninja.id}>
+      <div>Name: {ninja.name}</div>
+      <div>Age: {ninja.age}</div>
+      <div>Belt: {ninja.belt}</div>
+    </div>)
+  })
 
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: { ninja.name }</div>
-          <div>Age: { ninja.age }</div>
-          <div>Belt: { ninja.belt }</div>
-        </div>
-      )
-    })
-
-    return (
-      <div className="ninja-list">
-        { ninjaList }
-      </div>
-    );
-  }
+  return (<div className="ninja-list">
+    {ninjaList}
+  </div>);
 }
 
 export default Ninjas

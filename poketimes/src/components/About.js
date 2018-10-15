1ef0254f6a9ec1a5feb6b029099197d76fb573d6
jbeltrami <joao@primarydesign.com>
2018-10-15 15:44:16 -0400

    Use image in a react component
    - Import the image in the component that it is going to be used
    - Add src value in a variable format

--- poketimes/src/components/About.js
+++ poketimes/src/components/About.js
@@ -4,7 +4,7 @@ import Rainbow from '../hoc/Rainbow'
 const About = () => {
   return (
     <div className="container">
-      <h4 className="center">This is the About Page</h4>
+      <h4 className="center">About</h4>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores quisquam laudantium velit ea autem sequi animi neque repudiandae deleniti asperiores, repellat labore omnis sed architecto facere eius aliquam sint!</p>
     </div>
   )

2961d07cd4b61aad25d3badac38096c26563dc53
jbeltrami <joao@primarydesign.com>
2018-10-15 14:51:03 -0400

    Use axios to get data from a rest api.
    Use a conditional to check if data is available.
    Use the data to output cards in the ui.

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Link, NavLink, withRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
 
-  console.log(props)
-  setTimeout(() => {
-    props.history.push('/about')
-  }, 2000)
+  // console.log(props)
+  // setTimeout(() => {
+  //   props.history.push('/about')
+  // }, 2000)
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">

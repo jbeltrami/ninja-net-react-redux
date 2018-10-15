cf04d99be8d4a83033772cbf0f0672f56b74fad0
jbeltrami <joao@primarydesign.com>
2018-10-15 14:04:49 -0400

    Update class to className in jsx components
    - Use withRouter to pass props to navbar component

--- poketimes/src/components/Contact.js
+++ poketimes/src/components/Contact.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+  // setTimeout(() => {
+  //   props.history.push('/about')
+  // }, 2000)
+
   return (
-    <div class="container">
+    <div className="container">
       <h4 className="center">This is the Contact Page</h4>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores quisquam laudantium velit ea autem sequi animi neque repudiandae deleniti asperiores, repellat labore omnis sed architecto facere eius aliquam sint!</p>
     </div>
--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div class="container">
+    <div className="container">
       <h4 className="center">This is the Home Page</h4>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores quisquam laudantium velit ea autem sequi animi neque repudiandae deleniti asperiores, repellat labore omnis sed architecto facere eius aliquam sint!</p>
     </div>

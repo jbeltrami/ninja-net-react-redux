1e8d89d107afacf43ede92a773ea8db0077b7d3d
jbeltrami <joao@primarydesign.com>
2018-10-15 10:15:04 -0400

    Create components to test React Router.
    Next step: Prevent new routes from reloading the page.

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div class="container">
+      <h4 className="center">This is the Contact Page</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores quisquam laudantium velit ea autem sequi animi neque repudiandae deleniti asperiores, repellat labore omnis sed architecto facere eius aliquam sint!</p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div class="container">
+      <h4 className="center">This is the Home Page</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores quisquam laudantium velit ea autem sequi animi neque repudiandae deleniti asperiores, repellat labore omnis sed architecto facere eius aliquam sint!</p>
+    </div>
+  )
+}
+
+export default Home
